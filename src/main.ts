abd81dc295eb564af1ffa09dc7916d3378e3d1a2
Vladimir Pavković <vladimirpavk@gmail.com>
2018-01-05 10:46:23 +0100

    Working RxjS

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-/// <reference path="../node_modules/@types/rx/index.d.ts"/>
-
 'use strict'
 
 //import { ProbaKlasa, DerivedProba, StaticClass } from './app';
@@ -7,25 +5,4 @@ import * as mojModul from './app';
 //import { Observable } from 'rxjs';
 import * as RxJs from 'rxjs';
 
-
-
-/*
-let mojaKlasa=new ProbaKlasa();
-let derProba=new DerivedProba();
-*/
-let mojaKlasa=new mojModul.ProbaKlasa();
-let derProba=new mojModul.DerivedProba();
-
-console.log(derProba.getProbaText());
-
-let prom1=5;
-
-function addition(x:number, y:number):number{
-    return x+y+prom1;
-}
-
-/*import majKlasa from "./app";
-let mK=new majKlasa();*/
-let mk=new mojModul.default();
-
-console.log(addition(3,2));
+RxJs.Observable.of(1,2,3).subscribe(x=>console.log(x));
