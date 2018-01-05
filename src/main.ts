11f2602fea42da8f9c5731d858c61899fcf08ceb
Vladimir Pavković <vladimirpavk@gmail.com>
2018-01-05 14:50:08 +0100

    Vezbanje pocinje

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,6 @@ import * as mojModul from './app';
 //import { Observable } from 'rxjs';
 import * as RxJs from 'rxjs';
 
-RxJs.Observable.of(1,2,3).subscribe(x=>console.log(x));
+RxJs.Observable.of(1,2,3).subscribe(
+    x=>console.log(x)
+);
