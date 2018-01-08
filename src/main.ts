05637907381a8eafe7fda74ec3618e86f3a9206f
Vladimir Pavković <vladimirpavk@gmail.com>
2018-01-08 14:09:00 +0100

    Observables & Subjects

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,54 @@
 'use strict'
+import { Subject } from 'rxjs/Rx';
+import { queue } from 'rxjs/scheduler/queue';
 
 //import { ProbaKlasa, DerivedProba, StaticClass } from './app';
 import * as mojModul from './app';
 //import { Observable } from 'rxjs';
 import * as RxJs from 'rxjs';
 
-RxJs.Observable.of(1,2,3).subscribe(
-    x=>console.log(x)
+/*RxJs.Observable.of(1,2,3).subscribe(
+    x=>console.log(x),
+    error=>console.log(error),
+    ()=>console.log("Completed...")
 );
+
+
+RxJs.Observable.create((observer)=>{
+    observer.next("Neka vrednost");
+    setTimeout(function() {
+        observer.complete();
+    }, (1500));
+}).map((data)=>{
+    console.log("Data :" + data)
+    return data + "transform";
+}).
+subscribe(
+    x=>console.log(x),
+    error=>console.log(error),
+    ()=>console.log("Completed...")
+);*/
+
+let observable = RxJs.Observable.interval(1000);
+let observer = {
+    next: (value)=>{
+        console.log(value);
+    },
+    error: (greskaKod)=>{
+        console.log(greskaKod);
+    }
+};
+observable
+.map((data)=>{
+        return data;
+    }
+)
+.filter((value)=>{
+    if((value%2)==0) return true
+})
+.subscribe(observer);
+
+/*let subject=new RxJs.Subject();
+subject.subscribe(observer);
+subject.next('Neka vrednost');
+subject.error('Neka greska');*/
